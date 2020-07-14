32731ce9ab1113b8486b457677e28ad96585a818
Håkon Hægland <hakon.hagland@gmail.com>
2020-07-14 14:28:17 +0200

    initial commit

---
+++ lib/Travis/OSX/Test.pm
@@ -0,0 +1,32 @@
+package Travis::OSX::Test;
+
+use strict;
+use warnings;
+
+our $VERSION = '0.001';
+
+1;
+
+=head1 NAME
+
+Travis::OSX::Test - Module abstract placeholder text
+
+=head1 SYNOPSIS
+
+=for comment Brief examples of using the module.
+
+=head1 DESCRIPTION
+
+=for comment The module's description.
+
+=head1 AUTHOR
+
+Håkon Hægland <hakon.hagland@gmail.com>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2020 by Håkon Hægland.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
